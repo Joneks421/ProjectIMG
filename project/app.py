fc49ff3f317b50821887b79013581173974f44bc
Joneks421 <78646580+Joneks421@users.noreply.github.com>
2022-03-30 19:47:18 +0300

    Alpha V0.2
    - Много работы с БД

--- project/app.py
+++ project/app.py
@@ -1,11 +1,13 @@
 import json
 import firebase_admin
+import os
+from datetime import datetime, date, time
 
 from firebase_admin import credentials, firestore, storage
 
 from flask import Flask, render_template, request
 
-cred = credentials.Certificate("/db/custd-89037-firebase-adminsdk-m9pyz-518a531784.json")
+cred = credentials.Certificate("custd-89037-firebase-adminsdk-m9pyz-518a531784.json")
 firebase_admin.initialize_app(cred, {'storageBucket': 'custd-89037.appspot.com'})
 
 db = firestore.client()
@@ -29,20 +31,33 @@ def pic():
     # if request.method == "GET":
     #     print(request.get_json())
     if request.method == "POST":
-        print(request.form['title'])
+        print(1)
         title = request.form['title']
+
         f = request.files['image']
         f.save(title)
         ######## Файл#######
         blob = bucket.blob(title)
         blob.upload_from_filename(title)
         blob.make_public()
-        plink= blob.public_url
+        plink = blob.public_url
+        os.remove(title)
         #######db#############3
+        picid = None
         info = {'title': title, 'image': plink}
-        db.collection('pInfo').document().add(info)
+        db.collection('pInfo').add(info)
+        doc_ref = db.collection('pInfo').get()
+        for i in doc_ref:
+            picid = i.id
         print(request.files["image"])
-    return json.load({1: 0})
+        print(picid)
+        jinfo = json.dumps({
+            "id": picid,
+            "title": title,
+            "url": plink,
+            "timestamp": datetime.now().timestamp()})
+
+        return jinfo
 
 
 if __name__ == "__main__":
