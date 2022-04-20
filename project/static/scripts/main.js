17a1e538a2c1965e7ca64d42a3d2d33612e75377
Python286 <78646821+Python286@users.noreply.github.com>
2022-04-20 19:38:32 +0300

    Release 0.1

--- project/static/scripts/main.js
+++ project/static/scripts/main.js
@@ -11,22 +11,19 @@ for (let smoothLink of smoothLinks) {
     });
 };
 
-(function() {
-    // Add event listener
-    document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector("#parallax");
-    // Magic happens here
-    function parallax(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
-        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
-        console.log(x);
-        elem.style.backgroundPosition = x;
-    }
+const faq_btn = document.querySelector('.faq-p > p');
+const prev = document.querySelector('.btn-prev');
+prev.addEventListener('click', function (e) {
+    if (faq_btn.innerHTML === "Support") {
+        faq_btn.innerHTML = "FaQ";}
+    else {
+        faq_btn.innerHTML = "Support";}
+});
 
-})();
+const next = document.querySelector('.btn-next');
+next.addEventListener('click', function (e) {
+    if (faq_btn.innerHTML === "FaQ") {
+        faq_btn.innerHTML = "Support";}
+    else {
+        faq_btn.innerHTML = "FaQ";}
+});
