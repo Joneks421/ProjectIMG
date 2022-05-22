010148a297ad81069e16d2a80861183e6b8bdc53
Joneks421 <78646580+Joneks421@users.noreply.github.com>
2022-05-22 11:44:24 +0300

    123

--- project/static/scripts/solution.js
+++ project/static/scripts/solution.js
@@ -19,6 +19,8 @@ const burger = $(menu, '.burger');
 const newMode = $(menu, '.mode');
 const comments = $(menu, '.comments');
 const commentsTools = $(menu, '.comments-tools');
+const deletes = $(menu, '.delete')
+const deletesTools = $(menu, '.delete-tools')
 const menuToggle = $(commentsTools, '.menu__toggle', false);
 const draw = $(menu, '.draw');
 const drawTools = $(menu, '.draw-tools');
@@ -174,6 +176,7 @@ currentImage.addEventListener('load', beforeLoadImg);
 if (sessionStorage.getItem('imgId')) {
     imgId = sessionStorage.getItem('imgId');
     currentImage.src = sessionStorage.getItem('imgUrl');
+    currentImage.height= window.screen.height -150;
     menuUrl.value = sessionStorage.getItem('imgSharedLink');
 };
 
@@ -300,6 +303,15 @@ comments.addEventListener('click', () => {
     menu.dataset.state = 'selected';
     comments.dataset.state = 'selected';
     commentsTools.classList.remove('tool');
+    canvas.style.cursor = 'default';
+    canvas.addEventListener('click', addCommentEvent);
+    canvas.removeEventListener('mousemove', circle);
+});
+
+deletes.addEventListener('click', () => {
+    menu.dataset.state = 'selected';
+    deletes.dataset.state = 'selected';
+    deletesTools.classList.remove('tool');
     canvas.style.cursor = 'default';
     canvas.addEventListener('click', addCommentEvent);
     canvas.removeEventListener('mousemove', circle);
@@ -446,6 +458,7 @@ if (window.location.search) {
         comments.dataset.state = 'selected';
         commentsTools.classList.remove('tool');
         currentImage.addEventListener('load', () => {
+            currentImage.height= window.screen.height -150;
             resizeCanvasAndMask();
             checkImg = false;
             menu.dataset.state = 'selected';
@@ -483,7 +496,6 @@ if (window.location.search) {
 function wss() {
     connection = new WebSocket(`wss://neto-api.herokuapp.com/pic/${imgId}`);
     connection.addEventListener('message', event => {
-        console.log(JSON.parse(event.data));
         if (JSON.parse(event.data).event === 'pic') {
             if (JSON.parse(event.data).pic.mask) {
                 canvas.style.background = `url(${JSON.parse(event.data).pic.mask})`;
@@ -600,13 +612,11 @@ function tick() {
         } else {
             modeElements.forEach(el => {
                 if (el.dataset.state === 'selected') {
-                    console.log(el);
                     widthMode = parseInt(getComputedStyle(el).width);
                 };
             });
             toolsElements.forEach(el => {
                 if (!el.classList.contains('tool')) {
-                    console.log(el)
                     widthTool = parseInt(getComputedStyle(el).width);
                 };
             });
